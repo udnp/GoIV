d6f403c932a3a664493a7b4d79495cdea17d8022
udnp <udnp@sawdagger.nagoya>
2018-08-02 21:07:30 +0900

    [A] added normalizer for Japanese Hiragana/Katakana Letter Small characters.

--- app/src/main/java/com/kamron/pogoiv/utils/StringUtils.java
+++ app/src/main/java/com/kamron/pogoiv/utils/StringUtils.java
@@ -1,6 +1,9 @@
 package com.kamron.pogoiv.utils;
 
 import java.text.Normalizer;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringUtils {
 /**
@@ -11,6 +14,27 @@ public class StringUtils {
         s = Normalizer.normalize(s, Normalizer.Form.NFD);
         s = s.replaceAll("[\\p{M}]", "");
 
+        /* append more normalizers below for each locales, if needed */
+        String lang = Locale.getDefault().getLanguage();
+
+        if (Locale.getDefault().getLanguage().contains("ja")) {
+            s = toNormalSizeLetters(s);
+        }
+
+        return s;
+    }
+
+    private static String toNormalSizeLetters(String s) {
+        StringBuffer sb = new StringBuffer();
+        Pattern p = Pattern.compile("[ぁぃぅぇぉっゃゅょァィゥェォッャュョ]");
+        Matcher matcher = p.matcher(s);
+
+        while (matcher.find()) {
+            char c = matcher.group().charAt(0);
+            c = (char)(c + ('あ' - 'ぁ'));
+            matcher.appendReplacement(sb, String.valueOf(c));
+        }
+        s = matcher.appendTail(sb).toString();
         return s;
     }
 }
