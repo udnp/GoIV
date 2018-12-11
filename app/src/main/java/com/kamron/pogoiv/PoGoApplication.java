d660fd20cef14ec2526dbd8acaff124a63c9748e
udnp <udnp@sawdagger.nagoya>
2018-12-12 01:11:40 +0900

    [C] the trained data file for OCR is copied when GoIV launched, instead of Pokefly.
    Note: Currently this data is being improved, so that is needed to be overwritten every time GoIV launched.
    it should considers other better place to call this logic, for UI performance and UX.

--- app/src/main/java/com/kamron/pogoiv/PoGoApplication.java
+++ app/src/main/java/com/kamron/pogoiv/PoGoApplication.java
@@ -4,8 +4,11 @@ import android.app.Application;
 import android.support.v7.app.AppCompatDelegate;
 
 import com.kamron.pogoiv.pokeflycomponents.MovesetsManager;
+import com.kamron.pogoiv.utils.CopyUtils;
 import com.kamron.pogoiv.utils.CrashlyticsWrapper;
 import com.kamron.pogoiv.utils.FontsOverride;
+
+import java.io.File;
 
 import timber.log.Timber;
 
@@ -26,6 +29,18 @@ public class PoGoApplication extends Application {
         // Fonts overriding application wide
         FontsOverride.setDefaultFont(this, "DEFAULT", "fonts/Lato-Medium.ttf");
         FontsOverride.setDefaultFont(this, "SANS_SERIF", "fonts/Lato-Medium.ttf");
+
+        // copy the trained data file for OCR
+        // Note:Currently this data is being improved, so that is needed to be overwritten every time GoIV launched.
+        //      it should considers other better place to call this logic, for UI performance and UX.
+        File externalFilesDir = getExternalFilesDir(null);
+        if (externalFilesDir == null) {
+            externalFilesDir = getFilesDir();
+        }
+        String extDir = externalFilesDir.toString();
+//        if (!new File(extDir + "/tessdata/eng.traineddata").exists()) {
+        CopyUtils.copyAssetFolder(getAssets(), "tessdata", extDir + "/tessdata");
+//        }
 
         MovesetsManager.init(this);
     }
